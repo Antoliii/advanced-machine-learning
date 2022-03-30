5e8dee44701533020ffa741ad1889c96c5ce5ffe
Anton <Anton4linden@gmail.com>
2022-03-30 11:41:46 +0200

    question a

--- homeworkA.py
+++ homeworkA.py
@@ -1,5 +1,15 @@
 import torch
+import numpy as np
 from torch_geometric.datasets import Planetoid
 
 dataset = Planetoid(root='data/Planetoid', name='Cora')
-print('bajs')
+
+# number of nodes
+nTrainingNodes = np.count_nonzero(dataset.data.train_mask)
+nTestNodes = np.count_nonzero(dataset.data.test_mask)
+nValNodes = np.count_nonzero(dataset.data.val_mask)
+# semi-supervised learning can be used for node-level classification, which is basically
+# identifying the unlabeled nodes in the graph.
+# supervised learning can be used for graph-level classification where we try to predict
+# the node labels for the entire graph.
+
