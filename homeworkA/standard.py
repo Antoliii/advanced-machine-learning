3b6de96e38b8d5b7f0c6d20efa68177702c9b3a7
Anton <Anton4linden@gmail.com>
2022-04-13 17:00:41 +0200

    think it works

--- homeworkA/standard.py
+++ homeworkA/standard.py
@@ -4,22 +4,14 @@ import numpy as np
 from torch.nn import Linear
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import NormalizeFeatures
-from torch_geometric.nn import GCNConv
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
 
-def visualize(h, color, sub):
+def visualize(h, color):
     z = TSNE(n_components=2).fit_transform(h.detach().cpu().numpy())
-
-    fig, axs = plt.subplots(2, figsize=(10, 0))
-    #axs[sub].xticks([])
-    #axs[sub].yticks([])
-    fig.suptitle('Vertically stacked subplots')
-    axs[sub].scatter(z[:, 0], z[:, 1], s=70, c=color, cmap="Set2")
-
-    if sub == 1:
-        fig.show()
+    plt.scatter(z[:, 0], z[:, 1], s=50, c=color, cmap="Set2")
+    plt.show()
 
 
 dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
@@ -77,7 +69,7 @@ def test():
 
 # training with early stop
 previous_loss = 1e6
-n = 1
+n = 0
 for epoch in range(1, 201):
     new_loss = train()
     if previous_loss <= new_loss:
@@ -85,79 +77,18 @@ for epoch in range(1, 201):
 
         # count
         n += 1
-        if n == 10:
+        if n == 2:
             break
     else:
         previous_loss = new_loss
 
         # reset
-        n = 1
+        n = 0
 
 test_acc = test()
 print(f'Test Accuracy: {test_acc:.4f}')
-visualize(model(data.x), color=data.y, sub=0)
-
-
-class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels):
-        super().__init__()
-        torch.manual_seed(1234567)
-        self.conv1 = GCNConv(dataset.num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, dataset.num_classes)
-
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.conv2(x, edge_index)
-        return x
-
-
-model = GCN(hidden_channels=16)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-criterion = torch.nn.CrossEntropyLoss()
+visualize(model(data.x), color=data.y)
 
 
 
-def train():
-    model.train()
-    optimizer.zero_grad()  # Clear gradients.
-    out = model(data.x, data.edge_index)  # Perform a single forward pass.
-    loss = criterion(out[data.train_mask],
-                     data.y[data.train_mask])  # Compute the loss solely based on the training nodes.
-    loss.backward()  # Derive gradients.
-    optimizer.step()  # Update parameters based on gradients.
-    return loss
-
-
-def test():
-    model.eval()
-    out = model(data.x, data.edge_index)
-    pred = out.argmax(dim=1)  # Use the class with highest probability.
-    test_correct = pred[data.test_mask] == data.y[data.test_mask]  # Check against ground-truth labels.
-    test_acc = int(test_correct.sum()) / int(data.test_mask.sum())  # Derive ratio of correct predictions.
-    return test_acc
-
-
-# training with early stop
-previous_loss = 1e6
-n = 1
-for epoch in range(1, 201):
-    new_loss = train()
-    if previous_loss <= new_loss:
-        print(f'Epoch: {epoch:03d}, Loss: {previous_loss:.4f}')
-
-        # count
-        n += 1
-        if n == 10:
-            break
-    else:
-        previous_loss = new_loss
-
-        # reset
-        n = 1
-
-test_acc = test()
-print(f'Test Accuracy: {test_acc:.4f}')
-visualize(model(data.x, data.edge_index), color=data.y, sub=1)
 
