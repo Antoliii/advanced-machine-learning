02e6c8649c17c62720c32c89ffec86d6275a6741
Anton <Anton4linden@gmail.com>
2022-05-04 14:47:57 +0200

    please man

--- homeworkB/agentClass.py
+++ homeworkB/agentClass.py
@@ -4,6 +4,8 @@ import math
 import h5py
 import hashlib
 import itertools
+import time
+from itertools import compress
 
 # This file provides the skeleton structure for the classes TQAgent and TDQNAgent to be completed by you, the student.
 # Locations starting with # TO BE COMPLETED BY STUDENT indicates missing code that should be written by you.
@@ -37,22 +39,31 @@ class TQAgent:
         #  [col, orientation]
         self.actions = np.array(
             [
+                [0, 0],
                 [0, 1],
                 [0, 2],
                 [0, 3],
+                [1, 0],
                 [1, 1],
                 [1, 2],
                 [1, 3],
+                [2, 0],
                 [2, 1],
                 [2, 2],
-                [2, 3]
+                [2, 3],
+                [3, 0],
+                [3, 1],
+                [3, 2],
+                [3, 3]
             ]
         )
         self.Q = np.zeros((2**n_state, len(self.actions)))  # (s, a)
         self.IDs = np.chararray(shape=(2 ** n_state), itemsize=64)
         self.reward_tots = np.zeros(self.episode_count)
-        self.current_row = 0
-        self.action = 0
+        self.new_row = 0
+        self.Q_row = 0
+        self.Q_col = 0
+
 
 
     def fn_load_strategy(self,strategy_file):
@@ -77,14 +88,19 @@ class TQAgent:
         self.state[-len(self.gameboard.tiles):] = -1
         self.state[-self.gameboard.cur_tile_type] = 1
         self.state[:-4] = np.ndarray.flatten(self.gameboard.board)
-        id = hashlib.sha256(self.state).hexdigest().upper()
+        id = hashlib.md5(self.state).hexdigest()
 
+        decoded = self.IDs.decode()
 
-        if id not in self.IDs.decode():  # new state
-            self.IDs[self.current_row] = id
-            self.current_row += 1
-            if self.current_row == 5:
-                print(self.current_row)
+        if id not in decoded:  # new state
+            self.IDs[self.new_row] = id
+            self.Q_row = self.new_row
+            self.new_row += 1
+
+        else:
+            condit = decoded == id
+            self.Q_row = np.where(condit == True)[0][0]
+
 
 
     def fn_select_action(self):
@@ -105,20 +121,32 @@ class TQAgent:
         # You can use this function to map out which actions are valid or not
 
         if np.random.rand() < self.epsilon:
-            action = np.randint(low=0, high=10)
-            tile_x = self.actions[action][0]
-            tile_orientation = self.actions[action][1]
+            Q_col = np.random.randint(low=0, high=16)
+            tile_x = self.actions[Q_col][0]
+            tile_orientation = self.actions[Q_col][1]
             while self.gameboard.fn_move(tile_x, tile_orientation):
-                action = np.randint(low=0, high=10)
-                tile_x = self.actions[action][0]
-                tile_orientation = self.actions[action][1]
-            self.action = action
+                Q_col = np.random.randint(low=0, high=16)
+                tile_x = self.actions[Q_col][0]
+                tile_orientation = self.actions[Q_col][1]
 
         else:
-            tile_x = self.actions[0][0]
-            tile_orientation = self.actions[0][1]
-            self.action = 0
+            Q_col = np.where(self.Q[self.Q_row, :] == np.max(self.Q[self.Q_row, :]))[0]
+            # print(Q_col)
+            Q_col = Q_col[np.random.randint(low=0, high=len(Q_col))]
+            tile_x = self.actions[Q_col][0]
+            tile_orientation = self.actions[Q_col][1]
+            while self.gameboard.fn_move(tile_x, tile_orientation):
+                Q_col = np.where(self.Q[self.Q_row, :] == np.max(self.Q[self.Q_row, :]))[0]
+                Q_col = Q_col[np.random.randint(low=0, high=len(Q_col))]
+                tile_x = self.actions[Q_col][0]
+                tile_orientation = self.actions[Q_col][1]
 
+                self.Q[self.Q_row, self.Q_col] = - np.inf
+
+            # print(tile_x, tile_orientation)
+
+        #  store action
+        self.Q_col = Q_col
 
     
     def fn_reinforce(self,old_state,reward):
@@ -131,10 +159,12 @@ class TQAgent:
         # Useful variables: 
         # 'self.alpha' learning rate
 
-        old_action = self.action
+        old_action = self.Q_col
+
 
         self.Q[old_state, old_action] = self.Q[old_state, old_action] + self.alpha * (
-                    reward + np.max(self.Q[self.current_row, :]) - self.Q[old_state, old_action])
+                    reward + np.max(self.Q[self.Q_row, :]) - self.Q[old_state, old_action])
+
 
 
     def fn_turn(self):
@@ -145,9 +175,10 @@ class TQAgent:
             if self.episode%1000==0:
                 saveEpisodes=[1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000];
                 if self.episode in saveEpisodes:
+                    pass
                     # TO BE COMPLETED BY STUDENT
                     # Here you can save the rewards and the Q-table to data files for plotting of the rewards and the Q-table can be used to test how the agent plays
-                    np.savetxt(f'rewards{self.episode}.csv', self.reward_tots)
+                    # np.savetxt(f'rewards{self.episode}.csv', self.reward_tots)
             if self.episode>=self.episode_count:
                 raise SystemExit(0)
             else:
@@ -157,10 +188,8 @@ class TQAgent:
             self.fn_select_action()
             # TO BE COMPLETED BY STUDENT
             # Here you should write line(s) to copy the old state into the variable 'old_state' which is later passed to fn_reinforce()
-            id = hashlib.sha256(self.state).hexdigest().upper()
-            cond = self.IDs.decode() == id
-            old_state = np.where(self.IDs.decode() == id)
 
+            old_state = self.Q_row
 
             # Drop the tile on the game board
             reward=self.gameboard.fn_drop()
@@ -170,6 +199,7 @@ class TQAgent:
 
             # Read the new state
             self.fn_read_state()
+
             # Update the Q-table using the old state and the reward (the new state and the taken action should be stored as attributes in self)
             self.fn_reinforce(old_state,reward)
 
